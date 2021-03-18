5c87f19a92eef078c879bdc9a505dfac6bf7e9f5
huahuaeasi <huahua@163.com>
2021-03-18 20:55:18 +0800

    0318

--- 03/0318_m_dfs.js
+++ 03/0318_m_dfs.js
@@ -34,7 +34,7 @@ var maxAreaOfIsland = function (grid) {
 };
 
 var maxAreaOfIsland2 = function (grid) {
-  let max = 0, count = 0
+  let max = 0
   const dfs = (i, j) => {
     if (i < 0 || i > grid.length - 1 || j < 0 || j > grid[0].length - 1) {
       return 0
@@ -42,7 +42,6 @@ var maxAreaOfIsland2 = function (grid) {
     let cur = grid[i][j]
     if (cur) {
       grid[i][j] = 0
-      ++count
       return 1 + dfs(i - 1, j) + dfs(i + 1, j) + dfs(i, j + 1) + dfs(i, j - 1)
     }
   }
@@ -51,11 +50,44 @@ var maxAreaOfIsland2 = function (grid) {
     for (var j = 0; j < grid[0].length; j++) {
       const cur = grid[i][j]
       if (cur) {
-        count = 0
-        dfs(i, j, 0)
-        max = Math.max(max, count)
+        max = Math.max(max, dfs(i, j, 0))
       }
     }
   }
   return max
+};
+
+
+/**
+ * Definition for Employee.
+ * function Employee(id, importance, subordinates) {
+ *     this.id = id;
+ *     this.importance = importance;
+ *     this.subordinates = subordinates;
+ * }
+ */
+
+/**
+ * 690. 员工的重要性
+ * @param {Employee[]} employees
+ * @param {number} id
+ * @return {number}
+ */
+var GetImportance = function (employees, id) {
+  var map = new Map(), sum = 0
+  const dfs = (id) => {
+    const cur = map.get(id)
+    if (!cur) return
+    sum += cur[0]
+    const ids = cur[1]
+    for (var i = 0; i < ids.length; i++) {
+      dfs(ids[i])
+    }
+  }
+  for (var i = 0; i < employees.length; i++) {
+    const e = employees[i]
+    map.set(e.id, [e.importance, e.subordinates])
+  }
+  dfs(id)
+  return sum
 };
