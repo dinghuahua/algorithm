2427ba2428e2cba9a1fc77450450e8ac638046c5
huahuaeasi <huahua@163.com>
2021-04-02 21:43:18 +0800

    0402

--- 04/0401_m_dp.js
+++ 04/0401_m_dp.js
@@ -7,6 +7,57 @@
  * @param {number} K
  * @return {number}
  */
+// 题解
 var findCheapestPrice = function (n, flights, src, dst, K) {
-
+    // 某一个目的地 k次中转 的最低价格
+    let dp = new Array(n).fill(0).map(_ => new Array(K + 1).fill(Infinity))
+    K = Math.min(K, n - 2);
+    // 出发站是src的 end 直达价格最低
+    dp[src][0] = 0;
+    // console.log(dp)
+    for (const flight of flights) {
+        const start = flight[0], end = flight[1], price = flight[2];
+        if (start == src) {
+            // 初始化出发地为src 目的地为end 的直达最低价格
+            dp[end][0] = Math.min(dp[end][0], price);
+        }
+    }
+    // console.log(dp)
+    // k站中转次数，0站不需要中转直接直达，可以直接用dp的结果
+    for (let k = 1; k <= K; k++) {
+        for (const flight of flights) {
+            const start = flight[0], end = flight[1], price = flight[2];
+            // 为什么可以取dp[end][k - 1] 这样的话中转次数就不满足条件啊？？？？
+            // dp[end][k] 之前可能会有重复的数据，所以要取dp[end][k]数据
+            // dp[start][k - 1] + price 当前航线的出发地k-1次中间 + 当前航班的中转价格 = k次中转
+            // 为什么这里不判断 if (start == src) ？？？ 不是很能想通
+            dp[end][k] = Math.min(dp[end][k - 1], dp[end][k], dp[start][k - 1] + price);
+            // dp[end][k] = Math.min(dp[end][k], dp[start][k - 1] + price);
+        }
+    }
+    // console.log(dp)
+    return dp[dst][K] === Infinity ? - 1 : dp[dst][K]
 };
+// 题解一维
+var findCheapestPrice2 = function (n, flights, src, dst, K) {
+    let dist = new Array(n).fill(Infinity);
+    // Initialize direct flight value
+    dist[src] = 0;
+    for (const f of flights) {
+        if (f[0] == src) {
+            dist[f[1]] = f[2];
+        }
+    }
+
+    for (let i = 0; i < K; i++) {
+        for (const f of flights) {
+            const u = f[0], v = f[1], w = f[2];
+            if ((dist[u] !== Infinity) && (dist[u] + w < dp[v])) {
+                dp[v] = dist[u] + w;
+            }
+        }
+        dist = dp;
+    }
+
+    return dist[dst] === Infinity ? -1 : dist[dst];
+}
---
+++ 04/0402_m_dp.js
@@ -0,0 +1,32 @@
+/**
+ * 1314. 矩阵区域和
+ * @param {number[][]} mat
+ * @param {number} K
+ * @return {number[][]}
+ */
+var matrixBlockSum = function (mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]], K = 1) {
+    const m = mat.length, n = mat[0].length
+    let dp = new Array(m).fill(0).map(_ => new Array(n).fill(0))
+    let answer = new Array(m).fill(0).map(_ => new Array(n).fill(0))
+    // 从0，0开始到i，j位置矩形的和
+    // dp[i][j] = 上一行当前位置 + 当前行前一个位置 - 对角线的位置
+    dp[0][0] = mat[0][0]
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            dp[i][j] = (dp?.[i - 1]?.[j] ?? 0) + (dp[i]?.[j - 1] ?? 0) - (dp?.[i - 1]?.[j - 1] ?? 0) + mat[i][j]
+        }
+    }
+    console.log(dp)
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            const minI = i - K < 0 ? 0 : i - K,
+                maxI = i + K >= m ? m - 1 : i + K,
+                minJ = j - K < 0 ? 0 : j - K,
+                maxJ = j + K >= n ? n - 1 : j + K
+            console.log(dp[maxI][maxJ] - (dp[maxI]?.[minJ - 1] ?? 0) - (dp?.[minI - 1]?.[maxJ] ?? 0) + (dp?.[minI - 1]?.[minJ - 1] ?? 0))
+            answer[i][j] = dp[maxI][maxJ] - (dp[maxI]?.[minJ - 1] ?? 0) - (dp?.[minI - 1]?.[maxJ] ?? 0) + (dp?.[minI - 1]?.[minJ - 1] ?? 0)
+        }
+    }
+    console.log(answer)
+};
+matrixBlockSum()
