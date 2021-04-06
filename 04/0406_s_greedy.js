025f837381a166d09983976f543b6bd518b8477c
huahua <huahua@163.com>
2021-04-06 23:43:09 +0800

    fix

---
+++ 04/0406_s_greedy.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @param {number[]} cost
+ * @return {number}
+ */
+/**
+ * @param {string} s
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCost = function (s, cost) {
+  let min = 0
+  for (let i = 0; i < s.length; ) {
+    // 找最长相邻相同字母
+    let j
+    for (j = i + 1; j < s.length; j++) {
+      if (s[j] !== s[i]) break
+    }
+    if (i + 1 !== j) {
+      let temp = cost.slice(i, j).sort((a, b) => a - b)
+      temp.pop()
+      min = temp.reduce((pre, i) => pre + i, min)
+    }
+    i = j
+  }
+  return min
+}
